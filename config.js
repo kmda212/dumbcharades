e65dafd137adaa128bdb51c3598a212538580ffa
Kapil Raja Durga (from Dev Box) <kadurga@microsoft.com>
2026-03-21 17:50:45 +0530

    Saving Puzzles

--- config.js
+++ config.js
@@ -2,5 +2,10 @@ const CONFIG = {
   timer: 60,
   passLimit: 3,
   roundsPerTeam: 5,
-  scoring: { correct: 1, pass: 0 }
+  teams: ["Team 1", "Team 2"],
+  scoring: {
+    correct: 1,
+    pass: 0,
+    skip: 0
+  }
 };
